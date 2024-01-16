f99b4e00c045b01ca0cebb26f6e25928f0a95468
SoleMJS <139501506+SoleMJS@users.noreply.github.com>
2024-01-16 14:09:03 +0500

    feat:(app): Реализовал Вход и регистрацию в приложение на backend с использованием JWT

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const chalk = require('chalk')
 const path = require('path')
-const {
-	addNote,
-	getNotes,
-	removeNote,
-	saveNotes,
-} = require('./notes.controller')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const { addNote, getNotes, removeNote, updateNote } = require('./notes.controller')
+const { addUser, loginUser } = require('./users.controller')
+const auth = require('./middlewares/auth')
 
 const port = 3000
 const app = express()
@@ -14,55 +13,150 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'pages')
 
-app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'public')))
-app.use(
-	express.urlencoded({
-		extended: true,
-	})
-)
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get('/login', async (req, res) => {
+  res.render('login', {
+    title: 'Express App',
+    error: undefined
+  })
+})
+
+app.post('/login', async (req, res) => {
+  try {
+    const token = await loginUser(req.body.email, req.body.password);
+
+    res.cookie('token', token, { httpOnly: true });
+
+    res.redirect('/')
+  } catch (e) {
+    res.render('login', {
+      title: 'Express App',
+      error: e.message
+    })
+  }
+})
+
+app.get('/register', async (req, res) => {
+  res.render('register', {
+    title: 'Express App',
+    error: undefined
+  })
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    await addUser(req.body.email, req.body.password);
+
+    res.redirect('/login')
+  } catch (e) {
+    if (e.code === 11000) {
+      res.render('register', {
+        title: 'Express App',
+        error: 'Email is already registered'
+      })
+
+      return
+    }
+    res.render('register', {
+      title: 'Express App',
+      error: e.message
+    })
+  }
+})
+
+app.get('/logout', (req, res) => {
+  res.cookie('token', '', { httpOnly: true })
+
+  res.redirect('/login')
+})
+
+app.use(auth);
 
 app.get('/', async (req, res) => {
-	res.render('index', {
-		title: 'Express App',
-		notes: await getNotes(),
-		created: false,
-	})
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    userEmail: req.user.email,
+    created: false,
+    error: false
+  })
 })
 
 app.post('/', async (req, res) => {
-	await addNote(req.body.title)
-	res.render('index', {
-		title: 'Express App',
-		notes: await getNotes(),
-		created: true,
-	})
+  try {
+    await addNote(req.body.title, req.user.email)
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: true,
+      error: false
+    })
+  } catch (e) {
+    console.error('Creation error', e)
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: true
+    })
+  }
 })
 
 app.delete('/:id', async (req, res) => {
-	await removeNote(req.params.id)
-	res.render('index', {
-		title: 'Express App',
-		notes: await getNotes(),
-		created: false,
-	})
+  try {
+    await removeNote(req.params.id)
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: false
+    })
+  } catch (e) {
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: e.message
+    })
+  }
 })
 
 app.put('/:id', async (req, res) => {
-	const id = req.params.id
-	const newTitle = req.body.title
-	const notes = await getNotes()
-	const updatedNotes = notes.map(note =>
-		note.id === id ? { ...note, title: newTitle } : note
-	)
-	await saveNotes(updatedNotes)
-	res.render('index', {
-		title: 'Express App',
-		notes: await getNotes(),
-		created: false,
-	})
+  try {
+    await updateNote({ id: req.params.id, title: req.body.title })
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: false
+    })
+  } catch (e) {
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      userEmail: req.user.email,
+      created: false,
+      error: e.message
+    })
+  }
 })
 
-app.listen(port, () => {
-	console.log(chalk.green(`Server has been started on port ${port}...`))
+mongoose.connect(
+  'mongodb+srv://poalrom:Qweqwe123@cluster0.fmyuyj0.mongodb.net/notes?retryWrites=true&w=majority'
+).then(() => {
+  app.listen(port, () => {
+    console.log(chalk.green(`Server has been started on port ${port}...`))
+  })
 })
+
